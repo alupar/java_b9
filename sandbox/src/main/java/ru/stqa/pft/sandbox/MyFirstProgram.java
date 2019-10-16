9e64a999644eeab14606dd948c47817edd903f5c
alupar <36619247+alupar@users.noreply.github.com>
2019-10-16 20:51:01 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,24 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram{
-	
-	public static void main(String[] args){
-		System.out.println("Hello world!");
-	}
+public class MyFirstProgram {
+
+  public static void main(String[] args) {
+
+
+    Point p1 = new Point(-2, 2);
+    Point p2 = new Point(2, -3);
+
+    System.out.println("Вычислено с помощью функции " + distance(p1, p2));
+
+    System.out.println("Вычислено с помощью метода " + p1.distance(p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    int dx = p2.x - p1.x;
+    int dy = p2.y - p1.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
 
 }
+
