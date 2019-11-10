00a4ca13afe275bb87931cb4a551060aa7a598ab
alupar <36619247+alupar@users.noreply.github.com>
2019-11-10 21:19:07 +0300

    Лекция 5.5. Повсеместное использование уникальных идентификаторов объектов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -7,18 +7,22 @@ public class GroupData {
   private String footer;
 
   public GroupData withId(int id) {
+    this.id = id;
     return this;
   }
 
   public GroupData withName(String name) {
+    this.name = name;
     return this;
   }
 
   public GroupData withHeader(String header) {
+    this.header = header;
     return this;
   }
 
   public GroupData withFooter(String footer) {
+    this.footer = footer;
     return this;
   }
 
@@ -50,11 +54,14 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 }
