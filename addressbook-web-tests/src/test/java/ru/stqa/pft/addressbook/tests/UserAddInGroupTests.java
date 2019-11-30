b6544ce0a2905be9c94a7176cf78f1166746a0b6
alupar <36619247+alupar@users.noreply.github.com>
2019-12-01 01:09:51 +0300

    Лекция 8.2. Подготовка проекта для тестирования MantisBT

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserAddInGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserAddInGroupTests.java
@@ -2,10 +2,10 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.UserData;
-import ru.stqa.pft.addressbook.model.Users;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+import ru.stqa.pft.addressbook.model.UserData;
+import ru.stqa.pft.addressbook.model.Users;
 
 public class UserAddInGroupTests extends TestBase {
 
@@ -13,15 +13,13 @@ public class UserAddInGroupTests extends TestBase {
   private UserData userToAdd;
 
   @BeforeMethod
-  public void ensurePreconditions(){
+  public void ensurePreconditions() {
     app.goTo().userPage();
-    if(app.db().users().size() == 0){
-      app.user().create(new UserData().withFirstname("Ivan").withLastname("Ivan2").withNickname("Ivan3").withCompany("HomeWork").withAddress("Abakan")
-              .withMobile("89078922450").withHomePhone("89078922451").withWorkPhone("89078922452")
-              .withEmail("w@ww.ww").withEmail2("e@e.e").withEmail3("q@q.q"));
+    if (app.db().users().size() == 0) {
+      app.user().create(new UserData().withFirstname("Ivan").withLastname("Ivan2").withNickname("Ivan3").withCompany("HomeWork").withAddress("Abakan").withMobile("89078922450").withHomePhone("89078922451").withWorkPhone("89078922452").withEmail("w@ww.ww").withEmail2("e@e.e").withEmail3("q@q.q"));
     }
 
-    if (app.db().groups().size() == 0){
+    if (app.db().groups().size() == 0) {
       app.goTo().groupPage();
       app.group().create(new GroupData().withName("test111").withFooter("footer1").withHeader("header1"));
 
@@ -33,7 +31,7 @@ public class UserAddInGroupTests extends TestBase {
 
     for (UserData userToAdd1 : allusers) {
       Groups userGroups = userToAdd1.getGroups();
-      if(userGroups.size()!= allgroups.size()) {
+      if (userGroups.size() != allgroups.size()) {
         allgroups.removeAll(userGroups);
         groupToAdd = allgroups.iterator().next();
         userToAdd = userToAdd1;
@@ -41,10 +39,8 @@ public class UserAddInGroupTests extends TestBase {
       }
     }
 
-    if(groupToAdd==null){
-      UserData newuser = new UserData().withFirstname("Ivan7").withLastname("Ivan8").withNickname("Ivan9").withCompany("Company").withAddress("Omsk")
-              .withMobile("89078922455").withHomePhone("89078922454").withWorkPhone("89078922453")
-              .withEmail("w@ww.ww").withEmail2("e@e.e").withEmail3("q@q.q");
+    if (groupToAdd == null) {
+      UserData newuser = new UserData().withFirstname("Ivan7").withLastname("Ivan8").withNickname("Ivan9").withCompany("Company").withAddress("Omsk").withMobile("89078922455").withHomePhone("89078922454").withWorkPhone("89078922453").withEmail("w@ww.ww").withEmail2("e@e.e").withEmail3("q@q.q");
       app.user().create(newuser);
       Users after = app.db().users();
       newuser.withId(after.stream().mapToInt((g) -> (g).getId()).max().getAsInt());
@@ -54,7 +50,7 @@ public class UserAddInGroupTests extends TestBase {
   }
 
   @Test
-  public void testUserAddingToGroup(){
+  public void testUserAddingToGroup() {
     app.goTo().userPage();
     UserData userbefore = userToAdd;
     Groups usergroupsbefore = userToAdd.getGroups();
@@ -65,8 +61,8 @@ public class UserAddInGroupTests extends TestBase {
     Users allusersafter = app.db().users();
     UserData userafter = null;
 
-    for(UserData userafter1 : allusersafter){
-      if (userafter1.getId() == beforeid){
+    for (UserData userafter1 : allusersafter) {
+      if (userafter1.getId() == beforeid) {
         userafter = userafter1;
         break;
       }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeleteGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeleteGroupTests.java
@@ -2,42 +2,40 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.UserData;
-import ru.stqa.pft.addressbook.model.Users;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+import ru.stqa.pft.addressbook.model.UserData;
+import ru.stqa.pft.addressbook.model.Users;
 
 public class UserDeleteGroupTests extends TestBase {
   @BeforeMethod
-  public void ensurePreconditions(){
-    if(app.db().users().size() == 0){
-      app.user().create(new UserData().withFirstname("Ivan").withLastname("Ivan2").withNickname("Ivan3").withCompany("Home").withAddress("Moscow")
-              .withMobile("89078922456").withHomePhone("89078922457").withWorkPhone("89078922458")
-              .withEmail("w@ww.ww").withEmail2("e@e.e").withEmail3("q@q.q"));
+  public void ensurePreconditions() {
+    if (app.db().users().size() == 0) {
+      app.user().create(new UserData().withFirstname("Ivan").withLastname("Ivan2").withNickname("Ivan3").withCompany("Home").withAddress("Moscow").withMobile("89078922456").withHomePhone("89078922457").withWorkPhone("89078922458").withEmail("w@ww.ww").withEmail2("e@e.e").withEmail3("q@q.q"));
     }
 
-    if (app.db().groups().size() == 0){
+    if (app.db().groups().size() == 0) {
       app.goTo().groupPage();
       app.group().create(new GroupData().withName("test111").withFooter("footer1").withHeader("header1"));
     }
   }
 
   @Test
-  public void testUserDeleteGroup(){
+  public void testUserDeleteGroup() {
     app.goTo().userPage();
     Groups groupsbefore = app.db().groups();
     Users usersbefore = app.db().users();
     UserData removedUser = usersbefore.iterator().next();
 
-    for (UserData removedUser1 : usersbefore){
+    for (UserData removedUser1 : usersbefore) {
       Groups userGroups = removedUser1.getGroups();
-      if (userGroups.size() != 0){
+      if (userGroups.size() != 0) {
         removedUser = removedUser1;
         break;
       }
     }
 
-    if(removedUser.getGroups().size() == 0){
+    if (removedUser.getGroups().size() == 0) {
       GroupData groupToAdd = groupsbefore.iterator().next();
       app.user().addToGroup(removedUser, groupToAdd);
     }
@@ -46,8 +44,8 @@ public class UserDeleteGroupTests extends TestBase {
     Users allusersbefore = app.db().users();
     UserData userbefore = null;
 
-    for(UserData userbefore1 : allusersbefore){
-      if (userbefore1.getId() == beforeid){
+    for (UserData userbefore1 : allusersbefore) {
+      if (userbefore1.getId() == beforeid) {
         userbefore = userbefore1;
         break;
       }
@@ -59,8 +57,8 @@ public class UserDeleteGroupTests extends TestBase {
     Users allusersafter = app.db().users();
     UserData userafter = null;
 
-    for(UserData userafter1 : allusersafter){
-      if (userafter1.getId() == beforeid){
+    for (UserData userafter1 : allusersafter) {
+      if (userafter1.getId() == beforeid) {
         userafter = userafter1;
         break;
       }
